17bac98511e0195490881def4730a62b4fb3e16e
janvimehta25 <janvimehta103@gmail.com>
2023-03-17 21:11:27 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Authentication/Login";
+import SignUp from "./Pages/Authentication/SignUp";
+import Landing from "./Pages/Landing/Landing";
+import NavBar from "./Pages/P_Home/Navbar";
+import Sidebar from "./Pages/P_Home/Sidebar";
+import Personal from "./Pages/P_Home/Personal";
+import Appointment from "./Pages/P_Home/Appointment";
+import Reports from "./Pages/P_Home/Reports";
+import Chat from "./components/Chat";
+import Laborartory from "./Pages/P_Home/Laborartory";
+
+
+function App() {
+  return (
+    
+    <Router>
+      
+      <Routes>
+        
+        <Route path="/" element = {<Landing />} />
+        <Route path="/login" element = {<Login/>} />
+        <Route path="/signup" element = {<SignUp />} />
+        <Route path="/navbar" element = {<NavBar />}></Route>
+        <Route path="/sidebar" element = {<Sidebar />}></Route>
+        <Route path="/personal" element = {<Personal />}></Route>
+        <Route path="/appointment" element = {<Appointment />}></Route>
+        <Route path="/reports" element = {<Reports />}></Route>
+        <Route path="/labo" element = {<Laborartory />} />
+      </Routes>
+    </Router>
+    
+  );
+}
+
+export default App;
